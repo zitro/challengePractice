e1195a45bc8bcf87cc1ed1b72e92bca1644388b9
zitro <bryan@bryanortiz.com>
2018-08-16 14:18:18 -0400

    maxChar done

--- exercises/maxchar/index.js
+++ exercises/maxchar/index.js
@@ -5,6 +5,23 @@
 // maxChar("abcccccccd") === "c"
 // maxChar("apple 1231111") === "1"
 
-function maxChar(str) {}
+function maxChar(str) {
+	let obj = {};
+	let maxCount = 0;
+	let currentChar = '';
+
+	for (let char of str){
+		obj[char] = obj[char] + 1 || 1;
+	}
+
+	for (let e in obj){
+		if(obj[e] > maxCount ){
+			maxCount = obj[e];
+			currentChar = e;
+		}
+	}
+
+	return currentChar;
+}
 
 module.exports = maxChar;
